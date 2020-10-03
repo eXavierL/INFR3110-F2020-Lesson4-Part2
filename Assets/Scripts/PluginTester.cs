a61cb070577acc1756dfd6484d03f677c4b54f9a
Xavier <xavier.law@ontariotechu.net>
2020-10-03 12:33:01 -0400

    Unity & C++ Files

---
+++ Assets/Scripts/PluginTester.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+//Xavier Law, 100701325
+
+public class PluginTester : MonoBehaviour
+{
+    public GameObject cube;
+
+    private const string DLL_NAME = "InClassLecture4";
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct Vector3D
+    {
+        public float x;
+        public float y;
+        public float z;
+    }
+
+    [DllImport(DLL_NAME)]
+    private static extern int GetID();
+
+    [DllImport(DLL_NAME)]
+    private static extern void SetID(int id);
+
+    [DllImport(DLL_NAME)]
+    private static extern Vector3D GetPosition();
+
+    [DllImport(DLL_NAME)]
+    private static extern void SetPosition(float x, float y, float z);
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log(GetID());
+            SetID(911);
+
+            SetPosition(3.4f, 5.7f, 9.8f);
+            Debug.Log(GetPosition().x);
+            Debug.Log(GetPosition().y);
+            Debug.Log(GetPosition().z);
+
+            cube.transform.position = new Vector3(GetPosition().x, GetPosition().y, GetPosition().z);
+        }
+    }
+}
---
+++ Native C++/InClassLecture4/Vector2D.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef __VECTOR2D__
+#define __VECTOR2D__
+
+
+struct Vector2D
+{
+	float x;
+	float y;
+};
+
+#endif /*define (__Vector2D__)*/
+
